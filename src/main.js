e52bf8f5fbd35acab9d098d4aa4774c57a845f31
David Menger <david.menger@me.com>
2019-09-15 17:24:40 +0200

    code cleanup

--- src/main.js
+++ src/main.js
@@ -1,21 +1,13 @@
 'use strict';
 
-const { pool } = require('../lib/mssql');
+// const { pool } = require('../lib/mssql');
 
-const AttachmentCache = require('./AttachmentCache');
-
-
-const AttachmentCache = new AttachmentCache(pool);
-
-module.exports = { AttachmentCache};
-
-
-/*
 const StateStorage = require('./StateStorage');
 const BotTokenStorage = require('./BotTokenStorage');
 const ChatLogStorage = require('./ChatLogStorage');
 const BotConfigStorage = require('./BotConfigStorage');
 const NotificationsStorage = require('./NotificationsStorage');
+const AttachmentCache = require('./AttachmentCache');
 
 module.exports = {
     StateStorage,
@@ -25,5 +17,3 @@ module.exports = {
     AttachmentCache,
     NotificationsStorage
 };
-
-*/
